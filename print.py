da2026f614e3a6adf59f55430b215215e8446351
Fe <weiderui2@icloud.com>
2026-02-24 21:58:06 +0800

    print学习

---
+++ print.py
@@ -0,0 +1,19 @@
+#print的练习
+
+#输出字符串
+print("Hello World") #普通输出
+print("Hello\nWorld") #换行输出
+print("a", "b") #有空格
+print("a" + "b") #无空格
+
+#输出数字
+print(123) #普通
+print(1.23) #浮点
+print(1 + 3) #带计算的（后续学）
+
+#输出变量（后续学）
+a = 1
+b = "abc"
+print(a)
+print(b)
+print(a, b)
